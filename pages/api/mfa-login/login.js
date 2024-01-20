b4a96a9e459b8a7dd8f233b55a6fe22a264ada4e
Jonathan Rovner <jonrovner@gmail.com>
2024-01-20 09:09:44 -0300

    refactoring

--- pages/api/mfa-login/login.js
+++ pages/api/mfa-login/login.js
@@ -2,14 +2,15 @@ import axios from "axios";
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-      
+      //console.log("REQUEST BODY", req.body)
+
       const options = {
         method: 'POST',
         url: process.env.AUTH0_ISSUER_BASE_URL + '/oauth/token',
         headers: {'content-type': 'application/x-www-form-urlencoded'},
         data: new URLSearchParams({
           grant_type: 'password',
-          username: req.body.email,
+          username: req.body.username,
           password: req.body.password,
           client_id: process.env.AUTH0_CLIENT_ID,
           client_secret: process.env.AUTH0_CLIENT_SECRET,
@@ -21,27 +22,13 @@ export default async function handler(req, res) {
 
       axios.request(options).then(function (response) {
         //error response is expected;
+        //console.log("RESPONSE DATA: ", response.data)
 
       }).catch(function (error) {
-        
+        //console.log("ERROR GETTING MFA TOKEN", error);
         const { mfa_token } = error.response.data
-        const mfaListUrl = process.env.AUTH0_ISSUER_BASE_URL + '/mfa/authenticators';
-
-        try{
-            axios.get(mfaListUrl, {
-                headers: {authorization:'Bearer ' + mfa_token}
-            }).then( response => {
-                console.log(response.data)
-                res.status(200).json(response.data)
-            
-            
-            })
-
-        }catch (e){
-            console.log("error listin factors ", e.error);
-
-        }
-
+        //console.log("MFA TOKEN ", mfa_token);
+        res.status(200).json({token:mfa_token})
 
       });
 
--- pages/mfa-login.jsx
+++ pages/mfa-login.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function MFAlogin() {
+
+    const [mfa_token, setToken] = useState("")
+    const handleSubmit = (e) => {   
+        e.preventDefault();
+        const url = '/api/mfa-login/login'
+        axios.post(url, {
+            username:e.target[0].value,
+            password:e.target[1].value
+        }).then(response => setToken(response.data.token))
+    }
+
   return (
     <>
-      <form action="/api/mfa-login/login" method='POST'>
+    {mfa_token 
+    ? <div>{mfa_token}</div>
+    : <form action="/api/mfa-login/login" method='POST' onSubmit={handleSubmit}>
         <input type="text" name='email' />
         <input type="password" name='password' />
         <button type='submit'></button>
-      </form>
+      </form>}
+      
     </>
   );
 }
