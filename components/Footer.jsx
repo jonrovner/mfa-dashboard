0dd62e8c6491888b094df6259018a01f4bf6492b
Jonathan Rovner <jonrovner@gmail.com>
2024-05-11 07:31:11 -0300

    footer logout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useUser } from '@auth0/nextjs-auth0';
 
-const Footer = () => (
+const Footer = () =>{
+  const {user} = useUser();
+  return(
   <footer className="bg-light p-3 text-center" data-testid="footer">
     
     <p data-testid="footer-text">
       Sample project provided by <a href="https://auth0.com">Auth0</a>
     </p>
-    <a href='/api/auth/logout'><div className='btn btn-primary mt-2'>Logout</div></a>
+
+    {
+      user && <a href='/api/auth/logout'><div className='btn btn-primary mt-2'>Logout</div></a>
+
+    }
+
   </footer>
-);
+);}
 
 export default Footer;
